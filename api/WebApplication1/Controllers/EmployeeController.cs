7ed7c7773fcd807b9daec1056d77f0aaedce25af
EmanYehia258 <56773982+EmanYehia258@users.noreply.github.com>
2019-11-07 14:04:25 +0200

    first commit

---
+++ api/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("Api/Employee")]
+    public class EmployeeController : ApiController
+    {
+        MyDBEntities1 objEntity = new MyDBEntities1();
+        [HttpGet]
+        [Route("AllEmployeeDetails")]
+        public IQueryable<EmployeeDetail> GetEmaployee()
+        {
+            try
+            {
+                return objEntity.EmployeeDetails;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("GetEmployeeDetailsById/{employeeId}")]
+        public IHttpActionResult GetEmaployeeById(string employeeId)
+        {
+            EmployeeDetail objEmp = new EmployeeDetail();
+            int ID = Convert.ToInt32(employeeId);
+            try
+            {
+                objEmp = objEntity.EmployeeDetails.Find(ID);
+                if (objEmp == null)
+                {
+                    return NotFound();
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok(objEmp);
+        }
+
+        [HttpPost]
+        [Route("InsertEmployeeDetails")]
+        public IHttpActionResult PostEmaployee(EmployeeDetail data)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                objEntity.EmployeeDetails.Add(data);
+                objEntity.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+
+            return Ok(data);
+        }
+
+        [HttpPut]
+        [Route("UpdateEmployeeDetails")]
+        public IHttpActionResult PutEmaployeeMaster(EmployeeDetail employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                EmployeeDetail objEmp = new EmployeeDetail();
+                objEmp = objEntity.EmployeeDetails.Find(employee.ID);
+                if (objEmp != null)
+                {
+                    objEmp.Name = employee.Name;
+                    objEmp.Address = employee.Address;
+                    objEmp.Email = employee.Email;
+                    objEmp.DOB = employee.DOB;
+                    objEmp.Gender = employee.Gender;
+                    objEmp.Address = employee.Address;
+
+                }
+                int i = this.objEntity.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(employee);
+        }
+        [HttpDelete]
+        [Route("DeleteEmployeeDetails")]
+        public IHttpActionResult DeleteEmaployeeDelete(int id)
+        {
+            //int empId = Convert.ToInt32(id);  
+            EmployeeDetail emaployee = objEntity.EmployeeDetails.Find(id);
+            if (emaployee == null)
+            {
+                return NotFound();
+            }
+
+            objEntity.EmployeeDetails.Remove(emaployee);
+            objEntity.SaveChanges();
+
+            return Ok(emaployee);
+        }
+    }
+}
